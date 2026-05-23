6010fe9f89f66c3720ee249ad3fb717b4cb8ff1e
IxPrumxI <walidaqeel@gmail.com>
2026-05-23 05:48:36 +0300

    Update DiscordSRV#getTotalPlayerCount and %totalplayers% for 26.1

--- src/main/java/github/scarsz/discordsrv/DiscordSRV.java
+++ src/main/java/github/scarsz/discordsrv/DiscordSRV.java
@@ -90,10 +90,7 @@ import org.bstats.charts.AdvancedPie;
 import org.bstats.charts.DrilldownPie;
 import org.bstats.charts.SimplePie;
 import org.bstats.charts.SingleLineChart;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.Warning;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
@@ -122,6 +119,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.*;
 import java.util.concurrent.*;
@@ -490,10 +488,6 @@ public class DiscordSRV extends JavaPlugin {
             getLogger().severe("DiscordSRV failed to load properly: " + e.getMessage() + ". See " + github.scarsz.discordsrv.util.DebugUtil.run("DiscordSRV") + " for more information. Can't figure it out? Go to https://discordsrv.com/discord for help");
         });
         initThread.start();
-
-        if (Bukkit.getWorlds().size() > 0) {
-            playerDataFolder = new File(Bukkit.getWorlds().get(0).getWorldFolder().getAbsolutePath(), "/playerdata");
-        }
     }
 
     public void disablePlugin() {
@@ -2243,11 +2237,15 @@ public class DiscordSRV extends JavaPlugin {
         return responses;
     }
 
-    private static File playerDataFolder = null;
     public static int getTotalPlayerCount() {
-        if (playerDataFolder == null) return 0;
-        File[] playerFiles = playerDataFolder.listFiles(f -> f.getName().endsWith(".dat"));
-        return playerFiles != null ? playerFiles.length : 0;
+        if (Bukkit.getWorlds().isEmpty()) return 0;
+        World world = Bukkit.getWorlds().get(0);
+
+        File playerDataFolder = new File(world.getWorldFolder(), "/playerdata");
+        if (!playerDataFolder.exists()) playerDataFolder = new File(world.getWorldFolder(), "../../../players/data");
+
+        File[] files = playerDataFolder.listFiles(f -> f.getName().endsWith(".dat"));
+        return files != null ? files.length : 0;
     }
 
     /**
